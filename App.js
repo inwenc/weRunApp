aeb3a78a5db6760a9963690a1e8f79d9c342dee8
Yingwen <inwen.chen@gmail.com>
2021-09-19 17:14:35 -0600

    navigation screen working

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from "react";
-import { StyleSheet, Text, View, Dimensions, PanResponder } from "react-native";
+//import React, { Component } from "react";
+// // import { StyleSheet, Text, View, Dimensions, PanResponder } from "react-native";
 import MapView from "react-native-maps";
 import * as Location from "expo-location";
 import * as Permissions from "expo-permissions";
 
-export default class App extends React.Component {
+import * as React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  Button,
+  Dimensions,
+  PanResponder,
+} from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+class CheckPointsMap extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -74,3 +86,81 @@ const styles = StyleSheet.create({
     height: Dimensions.get("window").height,
   },
 });
+// const SecondScreen = () => {
+//   return <Text>Hola</Text>;
+// };
+// class SecondScreen extends React.Component {
+//   render() {
+//     return <Text>Hola</Text>;
+//   }
+// }
+
+// const RootStack = createNativeStackNavigator();
+// const RootStackScreen = () => {
+//   <RootStack.Navigator>
+//     <RootStack.Screen name="App" component={HomeScreen} />
+//   </RootStack.Navigator>;
+// };
+//   Home: { screen: HomeScreen }, });
+
+// export default function App() {
+//   return <NavigationContainer>{<RootStack />}</NavigationContainer>;
+// }
+
+// const AppNavigator = createDrawerNavigator(
+//   {
+//     Home: {
+//       screen: HomeScreen,
+//     },
+//     About: {
+//       screen: AboutScreen,
+//     },
+//     Contact: {
+//       screen: ContactScreen,
+//     },
+//   },
+//   {
+//     initialRouteName: "Home",
+//     contentOptions: {
+//       activeTintColor: "#e91e63",
+//     },
+//   }
+// );
+
+// const AppContainer = createAppContainer(AppNavigator);
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Welcome to WeRun</Text>
+      <Button
+        title="Choose your checkpoints on Map"
+        onPress={() => navigation.navigate("CheckPointsMap")}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="CheckPointsMap" component={CheckPointsMap} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
